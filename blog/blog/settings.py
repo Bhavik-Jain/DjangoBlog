83b4ebaf64de7eeb2597b2201dd4bcd9f777dd42
Bhavik-Jain <bhvaikjain@gmail.com>
2021-01-13 16:25:38 +0530

    Added django-heroku

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'x!q-q!vdiqdh3n85d9&wx7_9ab#+t%!fwn3bz$(vyluzzxl^w+'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -152,3 +153,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = "ap-south-1"
 
 AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+django_heroku.settings(locals())
