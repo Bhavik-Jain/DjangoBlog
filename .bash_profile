1e1c2751a5dda01b4de39e94e0bbbdd897ada042
Bhavik-Jain <bhvaikjain@gmail.com>
2021-01-13 14:26:07 +0530

    Initial Commit

---
+++ .bash_profile
@@ -0,0 +1,8 @@
+alias osu="Start Sublime\Text \3"
+alias prof="osu ~/.bash_profile"
+alias reprof=". ~/.bash_profile"
+
+#DJANGO VARS
+export AWS_ACCESS_KEY_ID="AKIAYO3R3L3DDSDEC7UA"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_STORAGE_BUCKET_NAME="django-blog-files"
---
+++ blog/firstblog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, get_object_or_404 
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, User
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # just a single line to order the post according to date 
+    paginate_by = 3
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # form you are trying to submit first take the instance and set the author equals to current user  
+        return super().form_valid(form)
+
+# If you wanted to restrict the access for CreateView to the users who has staff status and I found a way to do that, 
+# If you import staff_member_required decorator from django.contrib.admin.views.decorators, 
+# you can use that decorator on PostCreateView class in views.py. The code will be like below.
+
+# from django.contrib.admin.views.decorators import staff_member_required
+
+# @method_decorator(staff_member_required, name='dispatch')
+#  class ArticleCreateView(CreateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'blog/post_confirm_delete.html'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About Us'}) 
