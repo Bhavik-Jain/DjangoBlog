4c7d013df8532101afb1b5a40e57028921ada96e
Bhavik-Jain <bhvaikjain@gmail.com>
2021-01-13 15:16:47 +0530

    Updated Settings

--- .bash_profile
+++ .bash_profile
@@ -5,4 +5,4 @@ alias reprof=". ~/.bash_profile"
 #DJANGO VARS
 export AWS_ACCESS_KEY_ID="AKIAYO3R3L3DDSDEC7UA"
 export AWS_SECRET_ACCESS_KEY="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"
-export AWS_STORAGE_BUCKET_NAME="django-blog-files"
+export AWS_STORAGE_BUCKET_NAME="django-firstblog-files"
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x!q-q!vdiqdh3n85d9&wx7_9ab#+t%!fwn3bz$(vyluzzxl^w+'
+# SECRET_KEY = 'x!q-q!vdiqdh3n85d9&wx7_9ab#+t%!fwn3bz$(vyluzzxl^w+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bhavikdjangoblog.herokuapp.com']
 
 
 # Application definition
